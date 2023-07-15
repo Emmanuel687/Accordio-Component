7ff73e81167750564b4834ca07432bf02d6308e5
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-07-15 15:30:08 +0300

    Dev:-Added Toggle Button

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,13 +1,44 @@
 import React from "react";
 import AccordionItem from "./AccordionItem";
-import "./Style.css"
+import "./Style.css";
+import { useState } from "react";
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={i} />
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="Test One"
+        num="Text 1"
+        key="test 1"
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>
+            Break up UI into components Make components reusable Place state
+            efficiently.
+          </li>
+          <li>
+            Make components reusable
+          </li>
+          <li>
+            Place state efficiently
+          </li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
